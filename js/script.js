b4348d92ceb0f5987b878c2815ce1cc974acd48e
hisa <76601471+hisaaashi@users.noreply.github.com>
2022-10-30 23:13:33 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,44 +1,43 @@
 $(function() {
-// スムーススクロール
-jQuery('a[href^="#"]').click(function () {
-  let header = jQuery(".js-header").innerHeight();
-  let speed = 300;
-  let id = jQuery(this).attr("href");
-  let target = jQuery("#" == id ? "html" : id);
-  let position = jQuery(target).offset().top - header;
-  if ("fixed" !== jQuery(".js-header").css("position")) {
-    position = jQuery(target).offset().top;
-  }
-  if (0 > position) {
-    position = 0;
-  }
-  jQuery("html, body").animate(
-    {
-      scrollTop: position,
-    },
-    speed
-  );
-  return false;
-});
+	// スムーススクロール
+	jQuery('a[href^="#"]').click(function () {
+		let header = jQuery(".header").innerHeight();
+		let speed = 300;
+		let id = jQuery(this).attr("href");
+		let target = jQuery("#" == id ? "html" : id);
+		let position = jQuery(target).offset().top - header;
+		if ("fixed" !== jQuery(".header").css("position")) {
+			position = jQuery(target).offset().top;
+		}
+		if (0 > position) {
+			position = 0;
+		}
+		jQuery("html, body").animate(
+			{
+				scrollTop: position,
+			},
+			speed
+		);
+		return false;
+	});
 
-// トップへ戻る
-jQuery(".js-top").click(function () {
-  jQuery("body,html").animate(
-    {
-      scrollTop: 0,
-    },
-    300
-  );
-  return false;
-});
+	// トップへ戻る
+	jQuery(".to-top").click(function () {
+		jQuery("body,html").animate(
+			{
+				scrollTop: 0,
+			},
+			300
+		);
+		return false;
+	});
 
-// スクロール検知
-jQuery(window).on("scroll", function () {
-  if (100 < jQuery(this).scrollTop()) {
-    jQuery(".js-top").addClass("is-show");
-  } else {
-    jQuery(".js-top").removeClass("is-show");
-  }
-});
-
+	// スクロール検知
+	jQuery(window).on("scroll", function () {
+		if (100 < jQuery(this).scrollTop()) {
+			jQuery('body').attr('data-scroll', 'true');
+		} else {
+			jQuery('body').attr('data-scroll', 'false');
+		}
+	});
 })
